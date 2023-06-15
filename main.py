004c658900f556627de51f5e650d0b6fd1840d04
leoph4m <87742260+l3oph4m@users.noreply.github.com>
2023-06-15 15:10:22 +0700

    init

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import string
 import re
 import pandas as pd
 import csv
+
+import ormsgpack, decimal
+
 from datetime import datetime
 from time import localtime
+
+def default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return str(obj)
+    raise TypeError
 
 def filter_raw_message(text):
     try:
@@ -14,7 +22,7 @@ def filter_raw_message(text):
         found2 = re.search('"p":(.+?"}"])}', text).group(1)
         print(found)
         print(found2)
-        return found1, found2
+        return found, found2
     except AttributeError:
         print("error")
     
@@ -64,7 +72,7 @@ def generate_csv(a):
             print(xi)
             ind= int(xi[1])
             ts= datetime.fromtimestamp(float(xi[4])).strftime("%Y/%m/%d, %H:%M:%S")
-            employee_writer.writerow([ind, ts, float(xi[5]), float(xi[6]), float(xi[7]), float(xi[8]), float(xi[9])])
+            employee_writer.writerow([ind, ts, float(xi[5]), float(xi[6]), float(xi[7]), float(xi[8]), 'TODO'])
 # add txt output file            
 def create_output_file():
     now = localtime()
@@ -74,23 +82,23 @@ def create_output_file():
 
 # Initialize the headers needed for the websocket connection
 headers = json.dumps({
-    # 'Connection': 'upgrade',
-    # 'Host': 'data.tradingview.com',
-    'Origin': 'https://data.tradingview.com'
-    # 'Cache-Control': 'no-cache',
-    # 'Upgrade': 'websocket',
-    # 'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
-    # 'Sec-WebSocket-Key': '2C08Ri6FwFQw2p4198F/TA==',
-    # 'Sec-WebSocket-Version': '13',
-    # 'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.56',
-    # 'Pragma': 'no-cache',
-    # 'Upgrade': 'websocket'
+    'Connection': 'upgrade',
+    'Host': 'tradestation.fireant.vn',
+    'Origin':'https://fireant.vn',
+    'Cache-Control': 'no-cache',    
+    'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+    'Sec-WebSocket-Key': 'FZxL0JOV3fmKy/RLg79/iQ==',
+    'Sec-WebSocket-Version': '13',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'Pragma': 'no-cache',
+    'Upgrade': 'websocket'
 })
 
     
 # Then create a connection to the tunnel
 ws = create_connection(
-    'wss://data.tradingview.com/socket.io/websocket',headers=headers)
+#    'wss://data.tradingview.com/socket.io/websocket',headers=headers)
+'wss://tradestation.fireant.vn/quote?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoxODg5NjIyNTMwLCJuYmYiOjE1ODk2MjI1MzAsImNsaWVudF9pZCI6ImZpcmVhbnQudHJhZGVzdGF0aW9uIiwic2NvcGUiOlsiYWNhZGVteS1yZWFkIiwiYWNhZGVteS13cml0ZSIsImFjY291bnRzLXJlYWQiLCJhY2NvdW50cy13cml0ZSIsImJsb2ctcmVhZCIsImNvbXBhbmllcy1yZWFkIiwiZmluYW5jZS1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImludmVzdG9wZWRpYS1yZWFkIiwib3JkZXJzLXJlYWQiLCJvcmRlcnMtd3JpdGUiLCJwb3N0cy1yZWFkIiwicG9zdHMtd3JpdGUiLCJzZWFyY2giLCJzeW1ib2xzLXJlYWQiLCJ1c2VyLWRhdGEtcmVhZCIsInVzZXItZGF0YS13cml0ZSIsInVzZXJzLXJlYWQiXSwianRpIjoiMjYxYTZhYWQ2MTQ5Njk1ZmJiYzcwODM5MjM0Njc1NWQifQ.dA5-HVzWv-BRfEiAd24uNBiBxASO-PAyWeWESovZm_hj4aXMAZA1-bWNZeXt88dqogo18AwpDQ-h6gefLPdZSFrG5umC1dVWaeYvUnGm62g4XS29fj6p01dhKNNqrsu5KrhnhdnKYVv9VdmbmqDfWR8wDgglk5cJFqalzq6dJWJInFQEPmUs9BW_Zs8tQDn-i5r4tYq2U8vCdqptXoM7YgPllXaPVDeccC9QNu2Xlp9WUvoROzoQXg25lFub1IYkTrM66gJ6t9fJRZToewCt495WNEOQFa_rwLCZ1QwzvL0iYkONHS_jZ0BOhBCdW9dWSawD6iF1SIQaFROvMDH1rg', headers=headers)
 
 session= generateSession()
 print("session generated {}".format(session))
@@ -98,18 +106,53 @@ print("session generated {}".format(session))
 chart_session= generateChartSession()
 print("chart_session generated {}".format(chart_session))
 
+def encode_json(obj):
+    return json.dumps(obj) + chr(0x1E)
+
+def ws_on_message(ws, message: str):
+     print(message)
+
+def ws_on_open(ws):
+    ws.send(encode_json({
+        "protocol": "json",
+        "version": 1
+    }))
+
+    x = encode_json({
+        "type": 1,
+        "invocationId":"55555",
+        "target": "GetBars",
+        "arguments":["VN30","1","2023-6-13 9:0:0","2023-6-13 15:0:0"]
+    }) 
+    
+    ws.send(x)    
+
+    y = encode_json({
+        "type": 1,
+        "invocationId":"66666",
+        "target": "GetSymbols",
+        "arguments": [""]
+    }) 
+    
+    ws.send(y)  
+
+ws_on_open(ws)
+
 # Then send a message through the tunnel 
-sendMessage(ws, "set_auth_token", ["unauthorized_user_token"])
-sendMessage(ws, "chart_create_session", [chart_session, ""])
-sendMessage(ws, "quote_create_session", [session])
-sendMessage(ws,"quote_set_fields", [session,"ch","chp","current_session","description","local_description","language","exchange","fractional","is_tradable","lp","lp_time","minmov","minmove2","original_name","pricescale","pro_name","short_name","type","update_mode","volume","currency_code","rchp","rtc"])
-sendMessage(ws, "quote_add_symbols",[session, "NASDAQ:AAPL", {"flags":['force_permission']}])
-sendMessage(ws, "quote_fast_symbols", [session,"NASDAQ:AAPL"])
+# sendMessage(ws, "set_auth_token", ["unauthorized_user_token"])
+# sendMessage(ws, "set_access_token", ["eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoxODg5NjIyNTMwLCJuYmYiOjE1ODk2MjI1MzAsImNsaWVudF9pZCI6ImZpcmVhbnQudHJhZGVzdGF0aW9uIiwic2NvcGUiOlsiYWNhZGVteS1yZWFkIiwiYWNhZGVteS13cml0ZSIsImFjY291bnRzLXJlYWQiLCJhY2NvdW50cy13cml0ZSIsImJsb2ctcmVhZCIsImNvbXBhbmllcy1yZWFkIiwiZmluYW5jZS1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImludmVzdG9wZWRpYS1yZWFkIiwib3JkZXJzLXJlYWQiLCJvcmRlcnMtd3JpdGUiLCJwb3N0cy1yZWFkIiwicG9zdHMtd3JpdGUiLCJzZWFyY2giLCJzeW1ib2xzLXJlYWQiLCJ1c2VyLWRhdGEtcmVhZCIsInVzZXItZGF0YS13cml0ZSIsInVzZXJzLXJlYWQiXSwianRpIjoiMjYxYTZhYWQ2MTQ5Njk1ZmJiYzcwODM5MjM0Njc1NWQifQ.dA5-HVzWv-BRfEiAd24uNBiBxASO-PAyWeWESovZm_hj4aXMAZA1-bWNZeXt88dqogo18AwpDQ-h6gefLPdZSFrG5umC1dVWaeYvUnGm62g4XS29fj6p01dhKNNqrsu5KrhnhdnKYVv9VdmbmqDfWR8wDgglk5cJFqalzq6dJWJInFQEPmUs9BW_Zs8tQDn-i5r4tYq2U8vCdqptXoM7YgPllXaPVDeccC9QNu2Xlp9WUvoROzoQXg25lFub1IYkTrM66gJ6t9fJRZToewCt495WNEOQFa_rwLCZ1QwzvL0iYkONHS_jZ0BOhBCdW9dWSawD6iF1SIQaFROvMDH1rg"])
+# sendMessage(ws, "chart_create_session", [chart_session, ""])
+# sendMessage(ws, "quote_create_session", [session])
+# sendMessage(ws,"quote_set_fields", [session,"ch","chp","current_session","description","local_description","language","exchange","fractional","is_tradable","lp","lp_time","minmov","minmove2","original_name","pricescale","pro_name","short_name","type","update_mode","volume","currency_code","rchp","rtc"])
+# sendMessage(ws, "quote_add_symbols",[session, "HOSE:VN30", {"flags":['force_permission']}])
+# sendMessage(ws, "quote_fast_symbols", [session,"HOSE:VN30"])
 
 #st='~m~140~m~{"m":"resolve_symbol","p":}'
 #p1, p2 = filter_raw_message(st)
-sendMessage(ws, "resolve_symbol", [chart_session,"symbol_1","={\"symbol\":\"NASDAQ:AAPL\",\"adjustment\":\"splits\",\"session\":\"extended\"}"])
-sendMessage(ws, "create_series", [chart_session, "s1", "s1", "symbol_1", "1", 5000])
+
+# sendMessage(ws, "resolve_symbol", [chart_session,"symbol_1","={\"symbol\":\"HOSE:VN30\",\"adjustment\":\"splits\",\"session\":\"extended\"}"])
+# sendMessage(ws, "create_series", [chart_session, "s1", "s1", "symbol_1", "1", 10])
+
 #sendMessage(ws, "create_study", [chart_session,"st4","st1","s1","ESD@tv-scripting-101!",{"text":"BNEhyMp2zcJFvntl+CdKjA==_DkJH8pNTUOoUT2BnMT6NHSuLIuKni9D9SDMm1UOm/vLtzAhPVypsvWlzDDenSfeyoFHLhX7G61HDlNHwqt/czTEwncKBDNi1b3fj26V54CkMKtrI21tXW7OQD/OSYxxd6SzPtFwiCVAoPbF2Y1lBIg/YE9nGDkr6jeDdPwF0d2bC+yN8lhBm03WYMOyrr6wFST+P/38BoSeZvMXI1Xfw84rnntV9+MDVxV8L19OE/0K/NBRvYpxgWMGCqH79/sHMrCsF6uOpIIgF8bEVQFGBKDSxbNa0nc+npqK5vPdHwvQuy5XuMnGIqsjR4sIMml2lJGi/XqzfU/L9Wj9xfuNNB2ty5PhxgzWiJU1Z1JTzsDsth2PyP29q8a91MQrmpZ9GwHnJdLjbzUv3vbOm9R4/u9K2lwhcBrqrLsj/VfVWMSBP","pineId":"TV_SPLITS","pineVersion":"8.0"}])
 
 
@@ -118,9 +161,9 @@ a=""
 outfilename = create_output_file()
 while True:
     try:
-        result = ws.recv()
-        print(result)
-        # a=a+result+"\n"
+        result = ws.recv()   
+        print(result)     
+        #a=a+result+"\n"
         with open(outfilename,"a") as ww:
             ww.write(result)
             ww.close()
@@ -128,4 +171,4 @@ while True:
         print(e)
         break
     
-generate_csv(a)
+#generate_csv(a)
